e76cfe1756d7f0400ea2c70af27c1fb1393b507f
Aquiles Igarzabal <aqui.igarzabal@gmail.com>
2022-10-12 20:26:00 -0300

    index deleted

--- api/src/db.js
+++ api/src/db.js
@@ -31,7 +31,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/clubDB`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ClubDB`,
         { logging: false, native: false }
       );
 
