6a06946dada12cdd261e87234f03df0d64a894fe
Lucas Martin Luzzani <lucasluzzani@gmail.com>
2022-10-13 11:05:44 -0300

    redux persist, para conservar la info en el local storage

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,15 +1,26 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from '../reducer';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducer/reducer";
+import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "persist-key",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composeEnhancers =
-    (typeof window !== 'undefined' &&
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-    compose;
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(thunk)),
+  persistedReducer,
+  composeEnhancers(applyMiddleware(thunk))
 );
+const persistor = persistStore(store);
 
 export default store;
+export { persistor };
