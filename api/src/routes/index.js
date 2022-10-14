8aabf0dc147377daa2e993b0feb3ee278d397498
Diegomendo00 <diego_121197@hotmail.es>
2022-10-14 10:17:55 -0500

    index

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,18 +1,14 @@
 const { Router } = require("express");
 const router = Router();
-const products= require("../routes/middlewares/products")
-
+const products = require("../routes/middlewares/products");
 
 const groups = require("./middlewares/groups");
-const admin = require("./middlewares/home");
-const { prueba} = require("./controllers/utils")
-
+// const admin = require("./middlewares/home");
+const { prueba } = require("./controllers/utils");
 
 router.use("/group", groups);
-router.use("/Admin", admin); //AndresV - ¿no entiendo bien por qué esta linea?
-router.use("/home", home);
-
-
+// router.use("/Admin", admin); //AndresV - ¿no entiendo bien por qué esta linea?
+// router.use("/home", home);
 
 router.use("/products", products);
 //router.use("/groups", groups);
