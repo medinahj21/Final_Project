6b22c2252c5fdfdd9c1d030be691520aabd2af86
medinahj21 <medinaho21@gmail.com>
2022-10-13 20:18:13 -0500

    updating changes

--- api/src/routes/controllers/putControllers.js
+++ api/src/routes/controllers/putControllers.js
@@ -4,36 +4,36 @@ const { Sequelize, Model } = require("sequelize");
 const asyncUpdateProduct = async (req, res) => {
 
     const { name, price, description, image, modifiers, filter_tags, is_order, stock, state, payment_term } = req.body;
-    const { id } = req.params;
-    
-    try {
-        const result = await Product.findOne({
+  const { id } = req.params;
+
+  try {
+    const result = await Product.findOne({
             where: { id: id }
-        });
-        if (result) {
+    });
+    if (result) {
             await Product.update({
-                name,
-                price,
-                description,
-                image,
-                modifiers,
-                filter_tags,
-                is_order,
-                stock,
-                state,
-                payment_term,
+          name,
+          price,
+          description,
+          image,
+          modifiers,
+          filter_tags,
+          is_order,
+          stock,
+          state,
+          payment_term,
             }, {
-                where: {
+          where: {
                     id: id
-                }
+        }
             })
             res.status(200).json({ message: 'Updated successfully' })
-        } else {
+    } else {
             return res.status(400).json({ error: 'Product not found' });
-        }
-    } catch (error) {
-        console.log(error);
-        }
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 
@@ -61,6 +61,6 @@ const putGroups = async (req, res) => {
 
 
 module.exports = {
-    asyncUpdateProduct,
+  asyncUpdateProduct,
     putGroups
 }
