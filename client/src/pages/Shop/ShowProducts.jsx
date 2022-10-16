cffefc194d60a2e6acd9bf9c17f3ab9bdec01cb3
medinahj21 <medinaho21@gmail.com>
2022-10-16 13:57:27 -0500

    cambios

--- client/src/pages/Shop/ShowProducts.jsx
+++ client/src/pages/Shop/ShowProducts.jsx
@@ -1,5 +1,5 @@
-import React, { Fragment, useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+
 
 import ProductCard from "../../components/ProductCard/ProductCard";
 
