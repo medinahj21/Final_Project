c3843fae608fe51eb2b819d6fd2718c7dd11efcf
Lucas Martin Luzzani <lucasluzzani@gmail.com>
2022-10-19 11:48:58 -0300

    index

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_DATA || "http://localhost:3001";
+
+console.log(axios.defaults.baseURL);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
