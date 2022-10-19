bd247ecd68634168f6d07b134609730ca953c9a0
Anfevasa <47144203+Anfevasa@users.noreply.github.com>
2022-10-19 14:13:10 -0500

    fixing player creation

--- client/src/redux/actions/player.js
+++ client/src/redux/actions/player.js
@@ -3,8 +3,16 @@ import axios from "axios";
 
 export const createPlayer = (personalInfo) => {
   return async (dispatch) => {
-    await axios.post(`${axios.defaults.baseURL}/players/create`, personalInfo);
-    dispatch({ type: CREATE_PLAYER });
+    try {
+      let response = await axios.post(`${axios.defaults.baseURL}/players/create`, personalInfo);
+      dispatch({ type: CREATE_PLAYER });
+      return response
+      
+    } catch (error) { 
+      return {error:error.message}
+          
+    }
+    
   };
 };
 
