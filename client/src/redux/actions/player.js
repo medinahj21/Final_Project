780e87414de5c794966a944343461d44aabf24ae
Anfevasa <47144203+Anfevasa@users.noreply.github.com>
2022-10-19 10:58:34 -0500

    Empezando a crear getPlayers

--- client/src/redux/actions/player.js
+++ client/src/redux/actions/player.js
@@ -1,4 +1,4 @@
-import { CREATE_PLAYER } from "./actions";
+import { CREATE_PLAYER, GET_PLAYERS_FROM_DB } from "./actions";
 import axios from "axios";
 
 export const createPlayer = (personalInfo) => {
@@ -7,3 +7,10 @@ export const createPlayer = (personalInfo) => {
     dispatch({ type: CREATE_PLAYER });
   };
 };
+
+export const getPlayersFromDB = () => {
+  return async(dispatch)=>{
+    let players = await axios.get(`${axios.defaults.baseURL}/players`).data;
+    dispatch({type: GET_PLAYERS_FROM_DB, payload: players})
+  }
+}
