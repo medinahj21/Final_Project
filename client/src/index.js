2f6ae11791c9e8f67650b6572398c3f0795aa706
medinahj21 <medinaho21@gmail.com>
2022-10-18 20:30:43 -0500

    test

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
-
 import "./index.css";
+import dotenv from "dotenv";
+
+import axios from "axios";
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
