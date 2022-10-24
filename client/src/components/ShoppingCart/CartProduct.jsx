92d782d6e97502e6718d7b4e369155938773260c
Lucas Martin Luzzani <lucasluzzani@gmail.com>
2022-10-24 10:14:05 -0300

    estilos al carrito para que no se vea tan feo

--- client/src/components/ShoppingCart/CartProduct.jsx
+++ client/src/components/ShoppingCart/CartProduct.jsx
@@ -16,21 +16,29 @@ export default function ShoppingCart(prod) {
   };
 
   return (
-    <div>
-      <div>
-        <h3>{prod.prod.product.name}</h3>
-        <h3>${prod.prod.product.price}.00</h3>
-        <h5>Cantidad: {prod.prod.quant}</h5>
-        <button onClick={() => handleDecrementProduct(prod.prod.product.id)}>
-          {" "}
-          -{" "}
-        </button>
-        <button onClick={() => handleIncrementProduct(prod.prod.product.id)}>
-          {" "}
-          +{" "}
-        </button>
-        <h4>Subtotal: ${prod.prod.quant * prod.prod.product.price}</h4>
+    <div className="cart-container-prod">
+      <div className="cart-content">
+        <h4 className="cart__product-title">{prod.prod.product.name}</h4>
+        <span>$ {prod.prod.product.price}.00</span>
+        <span>Cant: {prod.prod.quant}</span>
+        <div className="cart__container-button">
+          <button
+            className="modify__button"
+            onClick={() => handleDecrementProduct(prod.prod.product.id)}
+          >
+            {" "}
+            -{" "}
+          </button>
+          <button
+            className="modify__button"
+            onClick={() => handleIncrementProduct(prod.prod.product.id)}
+          >
+            {" "}
+            +{" "}
+          </button>
+        </div>
       </div>
+      <h4>Subtotal: ${prod.prod.quant * prod.prod.product.price}</h4>
     </div>
   );
 }
--- client/src/components/ShoppingCart/ShoppingCart.jsx
+++ client/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,33 +1,33 @@
 import { useSelector } from "react-redux";
-import CartProduct from "./CartProduct"
+import CartProduct from "./CartProduct";
+import Modal from "../UI/Modal";
+import "./ShoppingCart.css";
 
 const ShoppingCart = () => {
-
   const productsInCart = useSelector((state) => state.shoppingCartReducer.cart);
 
   return (
-    <div>
-      <h2>Carrito de Compras</h2>
-      {productsInCart?.filter((item)=> item.quant !== 0).map((prod, index) => {
-        return (
-          <CartProduct
-            key= {index}
-            prod= {prod}
-          />
-        );
-      })}
-      <div>
-        <h2>Total</h2>
-        <h4>
-          ${" "}
-          {productsInCart?.reduce(
-            (a, item) => a + item.product.price * item.quant,
-            0
-          )}
-          .00
-        </h4>
+    <Modal>
+      <div className="cart_container">
+        <h3>Carrito de Compras</h3>
+        {productsInCart
+          ?.filter((item) => item.quant !== 0)
+          .map((prod, index) => {
+            return <CartProduct key={index} prod={prod} />;
+          })}
+        <div>
+          <h3>Total</h3>
+          <h4>
+            ${" "}
+            {productsInCart?.reduce(
+              (a, item) => a + item.product.price * item.quant,
+              0
+            )}
+            .00
+          </h4>
+        </div>
       </div>
-    </div>
+    </Modal>
   );
 };
 
