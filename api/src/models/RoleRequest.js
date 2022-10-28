0030b8a1f360b81c64bbee4d96f7220c3fdafdab
Diegomendo00 <diego_121197@hotmail.es>
2022-10-28 10:42:02 -0500

    groupid

--- api/src/models/RoleRequest.js
+++ api/src/models/RoleRequest.js
@@ -1,26 +1,29 @@
-const { DataTypes } = require('sequelize');
-
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define('roleRequest', {
-        id: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            primaryKey: true
-        },
-        newRole: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        userInfo:{
-            type: DataTypes.JSON,
-            allowNull: false
-        },
-        groupId:{
-            type: DataTypes.STRING,
-            allowNull: true,
-        }
-    },{
-        timestamps:false 
-    });
+  sequelize.define(
+    "roleRequest",
+    {
+      id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      newRole: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      userInfo: {
+        type: DataTypes.JSON,
+        allowNull: false,
+      },
+      groupId: {
+        type: DataTypes.UUID,
+        allowNull: true,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
 };
